b44e78d05341b5c85bf7a1d387260ab0af27882d
iamkunal2k <kunalkumbhare8@gmail.com>
2023-01-25 19:43:33 +0530

    fixed icons colors and selection

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,6 +9,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
   List<IconData> _icons = [
     FontAwesomeIcons.plane,
     FontAwesomeIcons.bed,
@@ -17,16 +18,26 @@ class _HomeScreenState extends State<HomeScreen> {
   ];
 
   Widget _buildIcon(int index) {
-    return Container(
-      height: 60.0,
-      width: 60.0,
-      decoration: BoxDecoration(
-          color: Theme.of(context).accentColor,
-          borderRadius: BorderRadius.circular(30.0)),
-      child: Icon(
-        _icons[index],
-        size: 25.0,
-        color: Theme.of(context).primaryColor,
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _selectedIndex = index;
+        });
+        print(_selectedIndex);
+      },
+      child: Container(
+        height: 60.0,
+        width: 60.0,
+        decoration: BoxDecoration(
+            color: _selectedIndex == index
+                ? Theme.of(context).accentColor
+                : Color(0xffe7ebee),
+            borderRadius: BorderRadius.circular(30.0)),
+        child: Icon(
+          _icons[index],
+          size: 25.0,
+          color: _selectedIndex == index ?Theme.of(context).primaryColor : Color(0xffb4b1c4),
+        ),
       ),
     );
   }
@@ -55,13 +66,14 @@ class _HomeScreenState extends State<HomeScreen> {
             height: 20.0,
           ),
           Row(
-            children: _icons
-            .asMap()
-            .entries
-            .map((MapEntry map) => _buildIcon(map.key),)
-            
-            .toList()
-          ),
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: _icons
+                  .asMap()
+                  .entries
+                  .map(
+                    (MapEntry map) => _buildIcon(map.key),
+                  )
+                  .toList()),
         ],
       )),
     );
